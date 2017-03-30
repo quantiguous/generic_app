c033e3234b8774f97376e56ef64cdca294b96450
akil <kataria.subs@gmail.com>
2017-03-30 16:05:40 +0530

    will_paginate and post

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def advanced_search_page_links
+    capture do
+      concat link_to_if @records.previous_page,  'Previous', "javascript:$('#page').val(#{@records.previous_page});$('form').submit()"
+      concat " "
+      concat link_to_if @records.next_page, 'Next', "javascript:$('#page').val(#{@records.next_page});$('form').submit()"
+    end
+  end
 end
